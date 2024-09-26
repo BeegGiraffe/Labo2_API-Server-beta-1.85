a110c8332b844f493bf5e21b797199544f7ab92e
GigaWrathWave <gabrielrondeau25@gmail.com>
2024-09-26 11:05:53 -0400

    benoit

--- server.js
+++ server.js
@@ -8,11 +8,22 @@ const server = createServer(async (req, res) => {
     console.log(req.method);
     let httpContext = await HttpContext.create(req, res);
     if (!handleCORSPreflight(httpContext))
+    {
+        if (!await handleRequest(req, res)) {
+            res.writeHead(404);
+            res.end();
+        }
         if (!handleStaticResourceRequest(httpContext))
             if (!await router.API_EndPoint(httpContext))
                 httpContext.response.notFound('this end point does not exist...');
+    }
 
 });
+async function handleRequest(req, res) {
+    if (! await handleBookmarksServiceRequest(req, res))
+        return false;
+    return true;
+}
 async function handleBookmarksServiceRequest(req, res) {
     if (req.url.includes("/api/bookmarks")) {
         const bookmarksFilePath = "./bookmarks.json";
@@ -22,110 +33,24 @@ async function handleBookmarksServiceRequest(req, res) {
         let id = extract_Id_From_Request(req);
         switch (req.method) {
             case 'GET':
-                if (isNaN(id)) {
-                    res.writeHead(200, { 'content-type': 'application/json' });
-                    res.end(bookmarksJSON);
-                } else {
-                    let found = false;
-                    for (let bookmark of bookmarks) {
-                        if (bookmark.Id === id) {
-                            found = true;
-                            res.writeHead(200, { 'content-type': 'application/json' });
-                            res.end(JSON.stringify(bookmark));
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        res.writeHead(404);
-                        res.end(`Error : The bookmark of id ${id} does not exist`);
-                    }
-                }
+                res.writeHead(400);
+                res.end(`test Get`);
                 break;
             case 'POST':
-                let newbookmark = await getPayload(req);
-                validStatus = validateBookmark(newbookmark);
-                if (validStatus == '') {
-                    let maxId = 0;
-                    bookmarks.forEach(bookmark => {
-                        if (bookmark.Id > maxId)
-                            maxId = bookmark.Id;
-                    });
-                    newbookmark.Id = maxId + 1;
-                    bookmarks.push(newbookmark);
-                    fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
-                    res.writeHead(201, { 'content-type': 'application/json' });
-                    res.end(JSON.stringify(newbookmark));
-                } else {
-                    res.writeHead(400);
-                    res.end(`Error: ${validStatus}`);
-                }
+                res.writeHead(400);
+                res.end(`test Post`);
                 break;
             case 'PUT':
-                let modifiedbookmark = await getPayload(req);
-                validStatus = validateBookmark(modifiedbookmark);
-                if (validStatus == '') {
-                    if (!isNaN(id)) {
-                        if (!('Id' in modifiedbookmark)) modifiedbookmark.Id = id;
-                        if (modifiedbookmark.Id == id) {
-                            let storedbookmark = null;
-                            for (let bookmark of bookmarks) {
-                                if (bookmark.Id === id) {
-                                    storedbookmark = bookmark;
-                                    break;
-                                }
-                            }
-                            if (storedbookmark != null) {
-                                storedbookmark.Name = modifiedbookmark.Name;
-                                storedbookmark.Phone = modifiedbookmark.Phone;
-                                storedbookmark.Email = modifiedbookmark.Email;
-                                fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
-                                res.writeHead(200);
-                                res.end();
-                            } else {
-                                res.writeHead(404);
-                                res.end(`Error: The bookmark of id ${id} does not exist.`);
-                            }
-                        } else {
-                            res.writeHead(409);
-                            res.end(`Error: Conflict of id`);
-                        }
-                    } else {
-                        res.writeHead(400);
-                        res.end("Error : You must provide the id of bookmark to modify.");
-                    }
-                } else {
-                    res.writeHead(400);
-                    res.end(`Error: ${validStatus}`);
-                }
+                res.writeHead(400);
+                res.end(`test Put`);
                 break;
             case 'DELETE':
-                if (!isNaN(id)) {
-                    let index = 0;
-                    let oneDeleted = false;
-                    for (let bookmark of bookmarks) {
-                        if (bookmark.Id === id) {
-                            bookmarks.splice(index, 1);
-                            fs.writeFileSync(bookmarksFilePath, JSON.stringify(bookmarks));
-                            oneDeleted = true;
-                            break;
-                        }
-                        index++;
-                    }
-                    if (oneDeleted) {
-                        res.writeHead(204); // success no content
-                        res.end();
-                    } else {
-                        res.writeHead(404);
-                        res.end(`Error: The bookmark of id ${id} does not exist.`);
-                    }
-                } else {
-                    res.writeHead(400);
-                    res.end("Error : You must provide the id of bookmark to delete.");
-                }
+                res.writeHead(400);
+                res.end(`test Delete`);
                 break;
             case 'PATCH':
                 res.writeHead(501);
-                res.end("Error: The endpoint PATCH api/bookmarks is not implemented.");
+                res.end(`test Patch`);
                 break;
         }
         return true;
