bf5dde7eed9e780f8368e14459ed7a321af4aa11
GigaWrathWave <gabrielrondeau25@gmail.com>
2024-10-03 12:13:06 -0400

    Benoit

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -1,5 +1,30 @@
 import Controller from "./Controller.js";
 
+factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+  isPrime(value) {
+    for (var i = 2; i < value; i++) {
+      if (value % i === 0) {
+        return false;
+      }
+    }
+    return value > 1;
+  }
+  findPrime(n) {
+    let primeNumber = 0;
+    for (let i = 0; i < n; i++) {
+      primeNumber++;
+      while (!isPrime(primeNumber)) {
+        primeNumber++;
+      }
+    }
+    return primeNumber;
+  }
+
 export default class MathsController extends Controller {
   constructor(HttpContext) {
     super(HttpContext);
@@ -11,21 +36,24 @@ export default class MathsController extends Controller {
       if (data["op"]) {
         let operator = data["op"];
         let x = data["x"];
+        let y = data["y"];
+        let n = data["n"];
         let reponse = null;
         if (operator == " ") {
           operator = "+";
         }
-        let y = data["y"];
-        let n = data["n"];
 
         if (data["x"] && isNaN(x)) {
-            this.HttpContext.response.JSON("X is invalid.");
+          this.HttpContext.response.JSON("X is invalid.");
         }
         if (data["y"] && isNaN(y)) {
-            this.HttpContext.response.JSON("Y is invalid.");
+          this.HttpContext.response.JSON("Y is invalid.");
         }
         if (data["n"] && isNaN(n)) {
-            this.HttpContext.response.JSON("N is invalid.");
+          this.HttpContext.response.JSON("N is invalid.");
+        }
+        if (data["x"] && data["y"] && data["n"]) {
+          this.HttpContext.response.JSON("To many parameters.");
         }
 
         if (isNaN(operator)) {
@@ -37,6 +65,14 @@ export default class MathsController extends Controller {
             reponse = parseInt(x) * parseInt(y);
           } else if (operator == "/" && !isNaN(x) && !isNaN(y)) {
             reponse = parseInt(x) / parseInt(y);
+          } else if (operator == "%" && !isNaN(x) && !isNaN(y)) {
+            reponse = parseInt(x) % parseInt(y);
+          } else if (operator == "!" && !isNaN(n)) {
+            reponse = factorial(n);
+          } else if (operator == "p" && !isNaN(n)) {
+            reponse = isPrime(n);
+          } else if (operator == "np" && !isNaN(n)) {
+            reponse = findPrime(n);
           }
           this.HttpContext.response.JSON(reponse);
         }
